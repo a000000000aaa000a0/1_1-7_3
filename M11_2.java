e4d46e9969300a20913e7ab0a06221f4d255905c
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-01-19 14:57:00 +0900

    Commit

--- M11_2.java
+++ M11_2.java
@@ -1,19 +1,41 @@
+// CalculationExecutionクラスを定義する。
+class CalculationExecution {
+    private int x;
+    private int y;
 
-class Calculation_Execution {
-    private int x ;
-    private int y ;
-
-}
-
-class main {
-    public static void main(String[] args) {
-        System.out.println("aaa");
-        
-
+    // CalculationExecution クラスのコンストラクタを定義する。
+    public CalculationExecution(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
+    // 合計値を取得するメソッド
+    public int sum() {
+        int res = 0;
+        for (int num = x; num <= y; num++) {
+            res += num;
+        }
+        return res;
+    }
+
+    // xの値を取得するメソッド
+    public int getX() {
+        return x;
+    }
+
+    // yの値を取得するメソッド
+    public int getY() {
+        return y;
+    }
 }
-
-
-    
-
+// M11_2mainクラスを定義する。
+class M11_2main {
+    public static void main(String[] args) {
+        // 計算実行インスタンスの生成
+        CalculationExecution calcExec = new CalculationExecution(100, 200);
+        // 合計値を取得
+        int total = calcExec.sum();
+        // 結果を表示
+        System.out.printf("%dから%dの合計値は%dです。\n", calcExec.getX(), calcExec.getY(), total);
+    }
+}
