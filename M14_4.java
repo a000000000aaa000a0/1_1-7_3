ef163a614728742a75b94624c887a0ec41a1fd30
a000000000aaa000a0 <ktq2390326@stu.o-hara.ac.jp>
2024-01-29 13:18:12 +0900

    Commit

--- M14_4.java
+++ M14_4.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-class M14_3 {
-    
+class M14_4 {
     public static void main(String[] args) {
         // 変数scanに入力してもらった文字列を代入する。
         Scanner scan = new Scanner(System.in, "Shift-JIS");
-        System.out.println("文字列を入力：");
+        System.out.println("１つ目文字列を入力：");
         // Scannerクラスのscan変数をStringBuilderクラスのインスタンス変数に代入する。
         StringBuilder x = new StringBuilder(scan.nextLine()); 
+        System.out.println("２つ目文字列を入力：");
+        StringBuilder y = new StringBuilder(scan.nextLine());
         
 
-        // lengthメソッドで変数xの文字数を調べ、文字数が１１字以上の場合はその分を表示しない。
-        if (x.length() >= 11) {
-            System.out.println(x.substring(0, 10));
-        // 文字数が１０字以下の場合すべて表示する。
+        // equalsメソッドでxとyが同じ文字列かどうか比較する。
+        if (x.equals(y)) {
+            System.out.println("同じ文字列です。");
         } else {
-            System.out.println(x);
+            System.out.println("違う文字列です。");
         }
         // 変数scanのScannerクラスを閉じる。
         // 書かなくてもいいが、処理が重くなるので一応書いておく。
@@ -22,7 +22,3 @@ class M14_3 {
 
     }
 }
-
-    
-    
-
